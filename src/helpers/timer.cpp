e83ec962083435478876962527165b1c666caafa
Giang <giang.lai@rwth-aachen.de>
2024-09-04 14:33:37 +0200

    Timer checks cpu time now instead of wall clock time

--- src/helpers/timer.cpp
+++ src/helpers/timer.cpp
@@ -1,52 +1,30 @@
 #include "timer.h"
 
-Timer::Timer(int time_limit_in_ms) : total_time(time_limit_in_ms) {}
+Timer::Timer() {}
 
 void Timer::start_timer() {
 	assert((!running, "Cannot start timer while it's running"));
 	running = true;
 	pausing = false;
-	most_recent_start = std::chrono::system_clock::now();
-}
-
-double Timer::get_remaining_time() const {
-	assert((running, "Cannot compute remaining time while timer is not running."));
-	return total_time - get_elapsed_time();
-}
-
-double Timer::get_elapsed_time() const {
-	assert((running, "Cannot compute elapsed time while timer is not running."));
-	if (pausing) {
-		return elapsed_time_until_last_pause;
-	}
-	else {
-		double elapsed_time_since_last_pause = double(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - most_recent_start).count());
-		return elapsed_time_until_last_pause + elapsed_time_since_last_pause;
-	}
+	most_recent_start = clock();
 }
 
 void Timer::pause_timer() {
 	assert((running, "Cannot pause timer while it's not running."));
 	assert((!pausing, "Cannot pause timer while it's already pausing."));
 	pausing = true;
-	elapsed_time_until_last_pause += std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - most_recent_start).count();
+	accumulated_cpu_time += clock() - most_recent_start;
 }
 
 void Timer::proceed_with_timer() {
 	assert((running, "Cannot proceed timer while it's not running."));
 	assert((pausing, "Cannot proceed timer while it's not pausing."));
 	pausing = false;
-	most_recent_start = std::chrono::system_clock::now();
+	most_recent_start = clock();
 }
 
-bool Timer::in_time() const {
-	return (running) ? get_remaining_time() >= 0 : true;
-}
-
-bool Timer::running_status() const {
-	return running;
-}
-
-int Timer::time_limit() const {
-	return total_time;
+double Timer::get_accumulated_cpu_time_in_ms(){
+	assert((running, "Timer has not been started yet"));
+	assert((pausing, "Timer is not paused!"));
+	return 1000 * (((double)accumulated_cpu_time)/CLOCKS_PER_SEC);
 }
--- src/helpers/timer.h
+++ src/helpers/timer.h
@@ -1,25 +1,20 @@
 #ifndef TIMER_H
 #define TIMER_H
-#include <chrono> 
 #include <cassert>
+#include <time.h>
 
 const int ONE_MINUTE = 60000;
 
 class Timer {
 public:
-	Timer(int time_limit_in_ms);
+	Timer();
 	void start_timer();
 	void pause_timer();
 	void proceed_with_timer();
-	double get_remaining_time() const;
-	double get_elapsed_time() const;
-	bool in_time() const;
-	bool running_status() const;
-	int time_limit() const;
+	double get_accumulated_cpu_time_in_ms();
 private:
-	std::chrono::system_clock::time_point most_recent_start;
-	int total_time;
-	double elapsed_time_until_last_pause{0};
+	clock_t most_recent_start{0};
+	clock_t accumulated_cpu_time{0};
 	bool running{false};
 	bool pausing{false};
 };
